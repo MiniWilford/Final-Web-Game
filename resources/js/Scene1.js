0dec1e695728573155e9b08effc71dce819394b0
MiniWilford <45856509+MiniWilford@users.noreply.github.com>
2023-12-05 16:06:02 -0500

    Added preload logic for Scene1 /  Move to Scene2 immediatly with better Scene2 name

--- resources/js/Scene1.js
+++ resources/js/Scene1.js
@@ -4,10 +4,19 @@ class Scene1 extends Phaser.Scene {
     }
   
     preload(){ 
+       // load all assets tile sprites
+       this.load.image("bg_1", "assets/bg-1.png");
+       this.load.image("bg_2", "assets/bg-2.png");
+       this.load.image("ground", "assets/ground.png");
+       // load spritesheet
+       this.load.spritesheet("player", "assets/bee.png",{
+         frameWidth: 37,
+         frameHeight: 39
+       });
 
     }
 
     create() { 
-        
+      this.scene.start("FirestLevel");
     }
 }
