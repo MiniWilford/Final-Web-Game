0f658d5dbd9f1e614b6613d674527f8c16201e0d
MiniWilford <45856509+MiniWilford@users.noreply.github.com>
2023-12-08 18:56:10 -0500

    Added References to tutorial placeholder assets to be called

--- resources/js/Scene1.js
+++ resources/js/Scene1.js
@@ -8,6 +8,9 @@ class preloadGame extends Phaser.Scene{
        this.load.image("bg_1", "resources/assets/bg-1.png");
        this.load.image("bg_2", "resources/assets/bg-2.png");
        this.load.image("ground", "resources/assets/ground.png");
+       // Load Pipes
+       this.load.image('pipeb', 'resources/assets/pipeb.png');
+       this.load.image('pipet', 'resources/assets/pipet.png');
        // load spritesheet
        this.load.spritesheet("player", "resources/assets/bee.png",{
          frameWidth: 37,
