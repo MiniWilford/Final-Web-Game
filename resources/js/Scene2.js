511aad711b1ee3844a3c9c2f6640d296d181ba25
MiniWilford <45856509+MiniWilford@users.noreply.github.com>
2023-12-09 01:23:45 -0500

    Added platforms and items to collect

--- resources/js/Scene2.js
+++ resources/js/Scene2.js
@@ -1,8 +1,12 @@
+let obstacleCount = 0;
+let canvasWidth = 384;
+let canvasHeight = 240;
+
 class playGame extends Phaser.Scene {
   constructor() {
     super("PlayGame");
   }
-    
+
     create() { 
       // makes sprite with the size of our game screen (tiled)
       this.bg_1 = this.add.tileSprite(0, 0, game.config.width, game.config.height, "bg_1");
@@ -25,13 +29,26 @@ class playGame extends Phaser.Scene {
       // tile is shorter, positioned it at the bottom of he screen
       this.ground.y = 20 * 16;
      
-
       // add the player
-      this.player = this.physics.add.sprite(game.config.width * 1.5, game.config.height / 1.25, "player");
+      this.player = this.physics.add.sprite(25, game.config.height / 1.75, "player");
 
-      // Add ground & player collision
-      //this.player.setCollideWorldBounds(true);  // Only goes to half of paralel screen
-      //this.physics.add.collider(this.player, this.ground);
+      // add the collectable item sprite
+      this.item = this.add.sprite(1100, 110, game.config.width, 48, "item")
+
+      // Add Obstacles and respective gaps
+      let obstacles = this.physics.add.staticGroup();
+      let xGap = 50;
+      let canvasWidth = 384;
+      let obstaclePos = canvasWidth+2*xGap
+      let pos = getRandom();
+
+      // bottom placable at 260+gap to height
+      obstacles.create(obstaclePos, pos[0], 'pipeb').setScale(1).refreshBody();
+      obstacles.create(obstaclePos, pos[1], 'pipet').setScale(1).refreshBody();
+
+
+      // Add Player Collision with Obstacles
+      this.physics.add.collider(this.player, obstacles, playerHit, null, game)
 
 
       // create an animation for the player known as "fly"
@@ -79,6 +96,43 @@ class playGame extends Phaser.Scene {
       this.bg_1.tilePositionX = this.myCam.scrollX * .3;
       this.bg_2.tilePositionX = this.myCam.scrollX * .6;
       this.ground.tilePositionX = this.myCam.scrollX;
-      
+
+      // Reset & Failure conditions when touching skybox / ground
+      if(this.player.y > Number(game.canvas.height)+100) {
+        console.log("y= ", this.player.y)
+        this.player.y -= 100;
+      }
+      //set lower Bounds
+      if(this.player.y < -200) {
+          console.log("y= ", this.player.y)
+          this.player.y += 200;
+      }
     }
+  }
+
+function getRandom() {
+  let gap = 150;
+  let safePadding = 25;
+  let min = Math.ceil(safePadding+gap/2);
+  let max = Math.floor(canvasHeight-safePadding-gap/2);
+  let ran =  Math.floor(Math.random() * (max - min + 1)) + min;
+  let rantop = ran-((gap/2)+260);
+  let ranbot = ran+((gap/2)+260);
+  console.log(ranbot,rantop)
+  return [ranbot, rantop]
+}
+
+let hitflag = false;
+function playerHit() {
+    if(hitflag) return
+    console.log("Player hit!!!!!!!!!")
+    hitflag=true;
+    setTimeout(playerDead, 200)
+}
+
+function playerDead() {
+    console.log("Player dead!!!!!!!!!")
+    let player = this.player
+    //player.setCollideWorldBounds(false);
+    let gameOver =  true;
 }
