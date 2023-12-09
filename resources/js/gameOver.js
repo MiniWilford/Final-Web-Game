17f02f2363cca99428db60f215785725e035ea8a
MiniWilford <45856509+MiniWilford@users.noreply.github.com>
2023-12-09 15:13:49 -0500

    Added preload for scene1/scene2

--- resources/js/gameOver.js
+++ resources/js/gameOver.js
@@ -4,6 +4,20 @@ class gameOver extends Phaser.Scene {
     }
 
     preload() {
+        // load all assets tile sprites
+        this.load.image("bg_1", "resources/assets/bg-1.png");
+        this.load.image("bg_2", "resources/assets/bg-2.png");
+        this.load.image("ground", "resources/assets/ground.png");
+        // Load Pipes
+        this.load.image('pipeb', 'resources/assets/pipeb.png');
+        this.load.image('pipet', 'resources/assets/pipet.png');
+        // Load Item
+        this.load.image('item', 'resources/assets/star.png');
+        // load spritesheet as player
+        this.load.spritesheet("player", "resources/assets/bee.png",{
+            frameWidth: 37,
+            frameHeight: 39
+        });
 
     }
 
@@ -12,6 +26,6 @@ class gameOver extends Phaser.Scene {
     }
 
     update() {
-        
+
     }
 }
